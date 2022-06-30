ae7773789da7b2386720bc7acfaa1a87a3d29538
verilye <connor.j.logan@outlook.com>
2022-07-01 00:12:02 +1000

    Successfully pulled data from web service and printed to console

---
+++ Controllers/PreloadDataController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace WebDevTechAss1.Controllers
+{
+    public class PreloadDataController
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task PreloadData()
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://coreteaching01.csit.rmit.edu.au/~e103884/wdt/services/customers/");
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+
+            }
+           catch(HttpRequestException e)
+           {
+                Console.WriteLine("\nException Caught!");	
+                Console.WriteLine("Message :{0} ",e.Message);
+                
+           }
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,52 @@
-﻿using WebDevTechAss1.Models;
+﻿using WebDevTechAss1.Controllers;
+using WebDevTechAss1.Models;
 
 namespace WebDevTechAss1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
         {
-            
-            User user = new User();
 
-            user.name = "Connor";
+            // For testing purposes,
+            // spitting out info from web service here
 
-            //Main menu ui 
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync("https://coreteaching01.csit.rmit.edu.au/~e103884/wdt/services/customers/");
+                response.EnsureSuccessStatusCode();
 
-            Console.WriteLine("--- "+ user.name +" ---");
-            Console.WriteLine("[1] Deposit");
-            Console.WriteLine("[2] Withdraw");
-            Console.WriteLine("[3] Transfer");
-            Console.WriteLine("[4] My Statement");
-            Console.WriteLine("[5] Logout");
-            Console.WriteLine("[6] Exit");
-            Console.WriteLine("\n");
-            Console.WriteLine("Enter an option:");
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+
+            }
+            catch(HttpRequestException e)
+            {
+                    Console.WriteLine("\nException Caught!");	
+                    Console.WriteLine("Message :{0} ",e.Message);
+                    
+            }
+               
+            // User user = new User();
+
+            // user.name = "Connor";
+
+            // //Main menu ui 
+
+            // Console.WriteLine("--- "+ user.name +" ---");
+            // Console.WriteLine("[1] Deposit");
+            // Console.WriteLine("[2] Withdraw");
+            // Console.WriteLine("[3] Transfer");
+            // Console.WriteLine("[4] My Statement");
+            // Console.WriteLine("[5] Logout");
+            // Console.WriteLine("[6] Exit");
+            // Console.WriteLine("\n");
+            // Console.WriteLine("Enter an option:");
         }
     }
 }
