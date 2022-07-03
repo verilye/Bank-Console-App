e4d61e819ab64994a9aceadcb1015d47c9517c37
verilye <connor.j.logan@outlook.com>
2022-07-03 13:01:54 +1000

    Connection string safety test

--- Program.cs
+++ Program.cs
@@ -17,20 +17,29 @@ namespace WebDevTechAss1
             // Preload user data if entries not found and load into database
 
             await client.PreloadData();
+
+            while(true)
+            {
+
+                // //Main menu ui 
+
+                //Console.WriteLine("--- "+ user.name +" ---");
+                Console.WriteLine("[1] Deposit");
+                Console.WriteLine("[2] Withdraw");
+                Console.WriteLine("[3] Transfer");
+                Console.WriteLine("[4] My Statement");
+                Console.WriteLine("[5] Logout");
+                Console.WriteLine("[6] Exit");
+                Console.WriteLine("\n");
+                Console.WriteLine("Enter an option:");
+
+            }
+
+            
                
             
 
-            // //Main menu ui 
-
-            // Console.WriteLine("--- "+ user.name +" ---");
-            // Console.WriteLine("[1] Deposit");
-            // Console.WriteLine("[2] Withdraw");
-            // Console.WriteLine("[3] Transfer");
-            // Console.WriteLine("[4] My Statement");
-            // Console.WriteLine("[5] Logout");
-            // Console.WriteLine("[6] Exit");
-            // Console.WriteLine("\n");
-            // Console.WriteLine("Enter an option:");
+           
         }
     }
 }
